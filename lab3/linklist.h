16442f89dceb0a2542230d8c7d6e4de8f3ef1903
shoppinggo <1226336313@qq.com>
2017-10-03 10:06:33 -0700

    menu and linklist

---
+++ lab3/linklist.h
@@ -0,0 +1,14 @@
+/* data struct and its operations */
+
+typedef struct DataNode
+{
+	char*   cmd;
+	char*   desc;
+	int     (*handler)();
+	struct  DataNode *next;
+}tDataNode;
+
+/* find a cmd in the linklist and return the datanode pointer */
+tDataNode* FindCmd(tDataNode * head,char * cmd);
+/* show all cmd in listlist */
+int  ShowAllCmd(tDataNode * head);
